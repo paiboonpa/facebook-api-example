1642aa8180c82c945c8a2f172d96998945de5f84
Paiboon <paiboonpa@gmail.com>
2018-08-09 13:36:42 +0700

    add post methos

--- page.php
+++ page.php
@@ -4,15 +4,20 @@ require_once __DIR__ . '/vendor/autoload.php'; // change path as needed
 
 $fb = new \Facebook\Facebook([
   'app_id' => '{your-app-id}',
-  'app_secret' => '{your-app-secret}',
+  'app_secret' => '{your-secret}',
   'default_graph_version' => 'v3.1',
   //'default_access_token' => '{access-token}', // optional
 ]);
 
-$fb->setDefaultAccessToken('{your-access-token}');
+$fb->setDefaultAccessToken('{your-page-access-token}');
+
+$a_post = array(
+    'message' => "My test post"
+);
 
 try {
   $response = $fb->get('/me');
+  //$response = $fb->post('/me/feed', $a_post); // post to page
 } catch(Exception $e) {
   // When validation fails or other local issues
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
